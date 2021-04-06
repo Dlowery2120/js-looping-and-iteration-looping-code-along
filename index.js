5612c3e8afaf1ac93e1fde77b6aae53f3c807539
Devante Lowery <dlowery2120@gmail.com>
2021-04-06 01:03:30 -0500

    commit tree 6ff826b5642f0e35fa523d67f4cc0f1dfd371519

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++)
+    return [ `Thank you, ${names[i]}, for the wonderful ${event} gift!` ]
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+let countup = 0;
+while (countup < 10) {
+  console.log(countup++);
+} 
+
+countDown(10); 
